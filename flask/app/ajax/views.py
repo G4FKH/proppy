28a071bbb401b35f295a7316b6879b557fb6b422
James Watson <jimwatson@mac.com>
2016-03-15 17:35:08 +0300

    Adds month selection to the web page.

--- flask/app/ajax/views.py
+++ flask/app/ajax/views.py
@@ -12,15 +12,22 @@ from . import ajax
 
 @ajax.route('/predict', methods=['POST'])
 def predict():
-    #print(request.form)
+    print(request.form)
+
+    sys_pwr = 10 * log10(float(request.form['sys_pwr'])/1000.0)
+    sys_year = int(request.form['sys_year'])
+    sys_month = int(request.form['sys_month'])
+
     tx_name = request.form['tx_name'].strip()
     tx_lat = float(request.form['tx_lat'])
     tx_lon = float(request.form['tx_lon'])
-    tx_pwr = 10 * log10(float(request.form['tx_pwr'])/1000.0)
+    tx_gain = float(request.form['tx_gain'])
 
     rx_name = request.form['rx_name'].strip()
     rx_lat = float(request.form['rx_lat'])
     rx_lon = float(request.form['rx_lon'])
+    rx_gain = float(request.form['rx_gain'])
+
 
     input_file = NamedTemporaryFile(mode='w+t', prefix="proppy_", suffix='.in', delete=False)
     input_file.write('PathName "Proppy Plot"\n')
@@ -28,21 +35,21 @@ def predict():
     input_file.write('Path.L_tx.lat {:.2f}\n'.format(tx_lat))
     input_file.write('Path.L_tx.lng {:.2f}\n'.format(tx_lon))
     input_file.write('TXAntFilePath "ISOTROPIC"\n')
-    input_file.write('TXGOS 2.16\n')
+    input_file.write('TXGOS {:.2f}\n'.format(tx_gain))
 
     input_file.write('PathRXName "{:s}"\n'.format(rx_name))
     input_file.write('Path.L_rx.lat {:.2f}\n'.format(rx_lat))
     input_file.write('Path.L_rx.lon {:.2f}\n'.format(rx_lon))
     input_file.write('RXAntFilePath "ISOTROPIC"\n')
-    input_file.write('RXGOS 2.16\n')
+    input_file.write('RXGOS {:.2f}\n'.format(rx_gain))
 
     input_file.write('AntennaOrientation "TX2RX"\n')
-    input_file.write('Path.year {:d}\n'.format(2016))
-    input_file.write('Path.month  {:d}\n'.format(3))
+    input_file.write('Path.year {:d}\n'.format(sys_year))
+    input_file.write('Path.month  {:d}\n'.format(sys_month))
     input_file.write('Path.hour 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24\n')
     input_file.write('Path.SSN {:d}\n'.format(42))
     input_file.write('Path.frequency 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30\n')
-    input_file.write('Path.txpower {:.2f}\n'.format(tx_pwr))
+    input_file.write('Path.txpower {:.2f}\n'.format(sys_pwr))
     input_file.write('Path.BW 3000.0\n')
     input_file.write('Path.SNRr 18.0\n')
     input_file.write('Path.Relr 90\n')
@@ -50,17 +57,16 @@ def predict():
     input_file.write('Path.Modulation "ANALOG"\n')
     input_file.write('Path.SorL "SHORTPATH"\n')
     input_file.write('RptFileFormat "RPT_BCR | RPT_OPMUF"\n')
-    input_file.write('LL.lat {:.2f}\n'.format(min(tx_lat, rx_lat)))
-    input_file.write('LL.lng {:.2f}\n'.format(min(tx_lon, rx_lon)))
-    input_file.write('LR.lat {:.2f}\n'.format(min(tx_lat, rx_lat)))
-    input_file.write('LR.lng {:.2f}\n'.format(max(tx_lon, rx_lon)))
-    input_file.write('UL.lat {:.2f}\n'.format(max(tx_lat, rx_lat)))
-    input_file.write('UL.lng {:.2f}\n'.format(min(tx_lon, rx_lon)))
-    input_file.write('UR.lat {:.2f}\n'.format(max(tx_lat, rx_lat)))
-    input_file.write('UR.lng {:.2f}\n'.format(max(tx_lon, rx_lon)))
+    input_file.write('LL.lat {:.2f}\n'.format(rx_lat))
+    input_file.write('LL.lng {:.2f}\n'.format(rx_lon))
+    input_file.write('LR.lat {:.2f}\n'.format(rx_lat))
+    input_file.write('LR.lng {:.2f}\n'.format(rx_lon))
+    input_file.write('UL.lat {:.2f}\n'.format(rx_lat))
+    input_file.write('UL.lng {:.2f}\n'.format(rx_lon))
+    input_file.write('UR.lat {:.2f}\n'.format(rx_lat))
+    input_file.write('UR.lng {:.2f}\n'.format(rx_lon))
     input_file.write('DataFilePath "{:s}"\n'.format(current_app.config['ITURHFPROP_DATA_PATH']))
     input_file.close()
-
 
     FNULL = open(os.devnull, 'w')
     output_file = NamedTemporaryFile(prefix="proppy_", suffix='.out')
@@ -70,6 +76,7 @@ def predict():
         output_file.name],
         stdout=FNULL,
         stderr=subprocess.STDOUT)
+    print(input_file.name)
     print(output_file.name)
     prediction = REC533Out(output_file.name)
     muf, mesh_grid, params = prediction.get_p2p_plot_data('REL')
--- flask/app/main/forms.py
+++ flask/app/main/forms.py
@@ -6,14 +6,21 @@ from flask_wtf.file import FileField
 
 
 class P2PForm(Form):
+    sys_pwr = DecimalField("Power (W)")
+    sys_traffic = SelectField("Traffic", choices=[('cw', 'CW'), ('ssb', 'SSB')])
+    sys_month = SelectField("Month", choices=[('1', 'Jan'), ('2', 'Feb'), ('3','Mar'),('4','Apr'),('5','May'),('6','Jun'),('7','Jul'),('8','Aug'),('9','Sep'),('10','Oct'),('11','Nov'),('12','Dec')])
+    sys_year = SelectField("Month", choices=[('2015', '2016'), ('2016', '2016'), ('2017','2017')])
+
     tx_name = StringField("Name")
     tx_lat = DecimalField("Latitude")
     tx_lon = DecimalField("Longitude")
-    tx_pwr = DecimalField("Power (W)")
+    tx_gain = DecimalField("Ant Gain")
+
 
     rx_name = StringField("Name")
     rx_lat = DecimalField("Latitude")
     rx_lon = DecimalField("Longitude")
+    rx_gain = DecimalField("Ant. Gain")
 
 
 class AreaForm(Form):
