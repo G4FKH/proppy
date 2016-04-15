f6bb3852e8901adc1ee8e957c056b743f5fb02ec
James Watson <jimwatson@mac.com>
2016-04-15 11:43:48 +0300

    Uses InputRequired to overcome failure to validate if we have zero values.

--- flask/app/main/forms.py
+++ flask/app/main/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, SelectField, TextAreaField, IntegerField, DecimalField, TextField
-from wtforms.validators import Required, Length, NumberRange
+from wtforms.validators import Required, Length, NumberRange, InputRequired
 from wtforms.widgets import TextArea, HiddenInput
 from flask_wtf.file import FileField
 
@@ -14,13 +14,13 @@ class P2PForm(Form):
     year = IntegerField(widget=HiddenInput(), validators=[Required(), NumberRange(min=1900, max=2050)])
 
     tx_name = StringField("Name")
-    tx_lat = DecimalField("Latitude", [Required(), NumberRange(-90, 90)])
-    tx_lon = DecimalField("Longitude", [Required(), NumberRange(-180, 180)])
+    tx_lat = DecimalField("Latitude", [InputRequired(), NumberRange(-90, 90)])
+    tx_lon = DecimalField("Longitude", [InputRequired(), NumberRange(-180, 180)])
     tx_gain = DecimalField("Ant Gain", [Required()])
 
     rx_name = StringField("Name")
-    rx_lat = DecimalField("Latitude", [Required(), NumberRange(-90, 90)])
-    rx_lon = DecimalField("Longitude", [Required(), NumberRange(-180, 180)])
+    rx_lat = DecimalField("Latitude", [InputRequired(), NumberRange(-90, 90)])
+    rx_lon = DecimalField("Longitude", [InputRequired(), NumberRange(-180, 180)])
     rx_gain = DecimalField("Ant Gain", [Required()])
 
 
