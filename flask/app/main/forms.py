a26dc7df3ed86c778a0d2f79b607385a7364fd8c
James Watson <jimwatson@mac.com>
2016-04-21 09:35:37 +0300

    Revised layout to make the traffic combo wide enough to accept additional text describing the input parameters in more detail.

--- flask/app/main/forms.py
+++ flask/app/main/forms.py
@@ -7,21 +7,21 @@ from flask_wtf.file import FileField
 
 class P2PForm(Form):
     sys_pwr = DecimalField("Pwr(W)", [NumberRange(1, 1000000000)])
-    sys_traffic = SelectField("Traffic", choices=[('cw', 'CW'), ('ssb', 'SSB')])
+    sys_traffic = SelectField("Traffic", choices=[('cw', 'CW (BW=500Hz / SNR=0dB)'), ('ssb', 'SSB (BW=3kHz / SNR=13dB)')])
     sys_plot_type = SelectField("Plot", choices=[('BCR', 'Reliability'), ('SNR', 'SNR'), ('E', 'Signal Strength')])
 
     month = IntegerField(widget=HiddenInput(), validators=[Required(), NumberRange(min=1, max=12)])
     year = IntegerField(widget=HiddenInput(), validators=[Required(), NumberRange(min=1900, max=2050)])
 
-    tx_name = StringField("Name")
+    tx_name = StringField("Site Name")
     tx_lat_field = DecimalField("Latitude", [InputRequired(), NumberRange(-90, 90)])
     tx_lng_field = DecimalField("Longitude", [InputRequired(), NumberRange(-180, 180)])
-    tx_gain = DecimalField("Ant Gain", [InputRequired()])
+    tx_gain = DecimalField("Ant Gain (dBi)", [InputRequired()])
 
-    rx_name = StringField("Name")
+    rx_name = StringField("Site Name")
     rx_lat_field = DecimalField("Latitude", [InputRequired(), NumberRange(-90, 90)])
     rx_lng_field = DecimalField("Longitude", [InputRequired(), NumberRange(-180, 180)])
-    rx_gain = DecimalField("Ant Gain", [Required()])
+    rx_gain = DecimalField("Ant Gain (dBi)", [Required()])
 
 
 class AreaForm(Form):
